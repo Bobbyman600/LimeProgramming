599661fdee2e291c38effb352ae06373eb119732
Michael <noobok933@gmail.com>
2025-04-14 21:04:16 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <string>
+void mfp() {
+std::cout << "sm (something..)" << std::endl;
+
+}
+
+
+void ml() {
+
+std::cout << "ml (sm)" << std::endl;
+
+std::string sm;
+std::cin >> sm;
+    if (sm == "sm") {
+        std::cout << "sm (something)" << std::endl;
+}
+ else
+ std::cout << "ERROR ML" << std::endl;
+
+
+
+
+}
